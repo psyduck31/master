5175890dab9928c73a73c29c8ed4b9b0026f5660
psyduck31 <47318921+psyduck31@users.noreply.github.com>
2019-02-20 22:53:10 +0300

    Add files via upload

---
+++ 1_laba_2zadanie.py
@@ -0,0 +1,14 @@
+import random
+
+number = random.randint(0, 100)
+fortune = False
+while fortune == False:
+    predict = int(input('Введите ваше число: '))
+    if predict == number:
+        print('Поздравляю! Вы выиграли!')
+        fortune = True
+    else:
+        if predict > number:
+            print('Загаданое число меньше!')
+        else:
+            print('Загаданое число больше!')
